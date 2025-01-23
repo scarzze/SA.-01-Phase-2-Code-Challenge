5a4ac29a6354817686160e3ca0bf9e0fb5bee272
scarzze <hoseabiwott8@gmail.com>
2025-01-23 08:48:51 +0300

    last

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-      '@styles': '/src/styles',
-    },
-    extensions: ['.js', '.jsx'], // Resolve both .js and .jsx files
-  },
-  server: {
-    port: 5173, // Optional: Customize your dev server port
-  },
-});
+})
